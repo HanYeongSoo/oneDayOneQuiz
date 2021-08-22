b6e36e98e4d6072c5eb2fed796a79aa757624905
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2021-08-22 15:46:56 +0900

    day7Quiz With_Myunggeun

---
+++ src/Quiz07.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Quiz07 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int num = 0;
+		num = input.nextInt();
+		
+		while(true) {
+			if (num % 2 != 0 && num % 3 != 0) {
+				System.out.println(num + "은 소수입니다.");
+				break;
+			}
+			else {
+				System.out.println(num + "은 소수가 아닙니다.");
+				break;
+			}
+		} 
+	}
+
+}
