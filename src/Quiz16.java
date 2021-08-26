8e4df6d1cc2700564d87c65c328fa460789e7015
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2021-08-26 18:02:49 +0900

    day16Quiz With_Myunggeun

---
+++ src/Quiz16.java
@@ -0,0 +1,22 @@
+
+public class Quiz16 {
+
+	public static void main(String[] args) {
+		/*
+		 * 		별 찍 기
+		 * 		*
+		 * 		**
+		 * 		***
+		 * 		****
+		 * 		*****
+		 */
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
